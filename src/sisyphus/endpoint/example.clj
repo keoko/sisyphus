992f92198f571ba7eb56fdac29a61f628b39b15c
natxo cabre <natxo.cabre@gmail.com>
2016-03-09 20:46:06 +0100

    init project with duct template

---
+++ src/sisyphus/endpoint/example.clj
@@ -0,0 +1,7 @@
+(ns sisyphus.endpoint.example
+  (:require [compojure.core :refer :all]))
+
+(defn example-endpoint [config]
+  (context "/example" []
+    (GET "/" []
+      "This is an example endpoint4")))
--- test/sisyphus/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns sisyphus.core-test
-  (:require [clojure.test :refer :all]
-            [sisyphus.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
